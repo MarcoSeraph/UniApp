6dc2de501bdde27fda8140f2057bcbb8ebf57702
MarcoSeraph <chenliibob@163.com>
2023-03-24 16:49:12 +0800

    Port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
@@ -22,4 +24,4 @@ app.register_blueprint(unallocated_bp)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
